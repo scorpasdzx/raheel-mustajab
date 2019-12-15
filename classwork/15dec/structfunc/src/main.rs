d20c499980b6a87e4ebfa273bef7edc272411e31
Wajid Ali <wajidali1987@gmail.com>
2019-12-15 11:51:46 +0500

    15dec final

--- classwork/15dec/structfunc/src/main.rs
+++ classwork/15dec/structfunc/src/main.rs
@@ -4,6 +4,11 @@ struct Student{
     sub2:i32
 }
 fn main() {
+    let name = String::from("wajid ali");
+    let sub1 = 80;
+    let sub2 = 65;
+    let stud7 = structmaker(name, sub1, sub2);
+    println!(" structmaker {} {} {}", stud7.name, stud7.sub1, stud7.sub2);
     let stud1 = Student{
         name: String::from("wajid"),
         sub1: 80,
@@ -20,3 +25,17 @@ fn studenttup(stud:&(String, i32, i32)){
 fn studentstr(student: &Student){
     println!(" Struct values are {} {} {}", student.name, student.sub1, student.sub2);
 }
+fn structmaker(name:String, sub1:i32, sub2:i32) ->Student{
+    /*let stud6 = Student{
+        name:name,
+        sub1:sub1,
+        sub2:sub2
+    }; or */
+    let stud6 = Student{
+        name,
+        sub1,
+        sub2
+    }; 
+    stud6
+}// study tuple type struct    e.g struct stud(String,i32,i32);  semicolong is necessary when defining tupple type struct
+//random github match struct implement for homework
